adec62975b8761e946f5f884f4098aad42094fca
MaiiaKatts <132450421+MaiiaKatts@users.noreply.github.com>
2023-09-27 22:44:45 +0300

    add second project

---
+++ PostgreSQLProject/src/test/java/lesson6/PostgreSqlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lesson6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgreSqlProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
